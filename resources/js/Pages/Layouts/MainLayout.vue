a26cbdf3ecc1f17007f896473c132024f9cc24b6
Golvan98 <gilvinsz@gmail.com>
2024-01-24 07:19:56 -0500

    installed tailwindcss and fixed dependency issues & added design template reference in project folder

---
+++ resources/js/Pages/Layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+
+<div class="w-full h-screen bg-green-300">
+
+
+dads
+
+</div>
+
+
+
+</template>
+
+
+<script setup>
+
+
+</script>
---
+++ resources/js/Pages/firstpage.vue
@@ -0,0 +1,19 @@
+<template>
+
+<div class="w-full h-screen bg-green-300 text-red-500">
+
+
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+dads
+
+</div>
+
+
+</template>
+
+
+
+<script setup>
+</script>
