fb63ab411ca89e948ee70220cb0ba3e858352a18
Golvan98 <gilvinsz@gmail.com>
2024-02-05 06:27:54 -0500

    polished register ui, made some spacing tweaks to login modal as well

--- resources/js/Pages/Modals/loginModal.vue
+++ resources/js/Pages/Modals/loginModal.vue
@@ -1,6 +1,7 @@
 <template>
   <div  class="fixed inset-0 z-50 flex items-center justify-center bg-gray-950 bg-opacity-95 w-full ">
-        <div class="bg-white w-1/4 h-1/2 flex flex-col items-center justify-center text-gray-600 border border-gray-900 ">
+        <div class="bg-white w-1/4 h-2/5 flex flex-col items-center justify-center text-gray-600 border border-gray-900 ">
+
             <div id="closeSection" class="w-full h-10-percent flex items-start justify-end"> 
                 <button @click="closeLogModal" class=" text-black hover:text-red-400 focus:outline-none">
                     <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -9,7 +10,7 @@
                 </button>
             </div>
 
-            <div class="w-full h-20-percent flex items-center justify-center sm:text-xs md:text-md lg:text-4xl"> 𝔾ℙ𝕄</div>
+            <div class="w-full h-10-percent flex items-center justify-center sm:text-xs md:text-md lg:text-4xl"> 𝔾ℙ𝕄</div>
            <div id="2ndBlock" class="w-4/5 h-70-percent flex flex-col items-center justify-center">
             
     
@@ -19,7 +20,7 @@
             <input id="password" type="password" placeholder="password" class="bg-gray-300 w-4/5 h-10-percent py-4 mx-auto mt-3 mb-2 text-xs"> 
 
             <div class="w-4/5 flex items-center justify-center mt-1 mb-4"> Login  </div>
-            <div class=" flex items-center justify-center"> Register </div>
+           
 
            </div>
 
--- resources/js/Pages/Modals/registerModal.vue
+++ resources/js/Pages/Modals/registerModal.vue
@@ -1,6 +1,6 @@
 <template>
     <div  class="fixed inset-0 z-50 flex items-center justify-center bg-gray-950 bg-opacity-95 w-full ">
-          <div class="bg-white w-1/4 h-1/2 flex flex-col items-center justify-center text-gray-600 border border-gray-900 ">
+          <div class="w-1/4 flex flex-col items-center justify-center text-gray-600 border border-gray-900 bg-white h-2/5">
               <div id="closeSection" class="w-full h-10-percent flex items-start justify-end"> 
                   <button @click="closeRegModal" class=" text-black hover:text-red-400 focus:outline-none">
                       <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -9,16 +9,13 @@
                   </button>
               </div>
   
-              <div class="w-full h-20-percent flex items-center justify-center sm:text-xs md:text-md lg:text-4xl"> 𝔾ℙ𝕄</div>
+              <div class="w-full h-10-percent flex items-center justify-center sm:text-xs md:text-md lg:text-4xl"> 𝔾ℙ𝕄</div>
              <div id="2ndBlock" class="w-4/5 h-70-percent flex flex-col items-center justify-center">
               
-      
-              <input id="user" type="text" placeholder="username" class="bg-gray-300 w-4/5 h-10-percent py-4 mx-auto text-xs"> 
-  
-              
-              <input id="password" type="password" placeholder="password" class="bg-gray-300 w-4/5 h-10-percent py-4 mx-auto mt-3 mb-2 text-xs"> 
-  
-              <div class="w-4/5 flex items-center justify-center mt-1 mb-4"> Register  </div>
+              <input id="user" type="text" placeholder="email" class="bg-gray-300 w-4/5 h-10-percent py-4 mx-auto text-xs"> 
+              <input id="password" type="text" placeholder="username" class="bg-gray-300 w-4/5 h-10-percent py-4 mx-auto mt-3 mb-2 text-xs"> 
+              <input id="password" type="text" placeholder="password" class="bg-gray-300 w-4/5 h-10-percent py-4 mx-auto mt-1 mb-2 text-xs"> 
+          
               <div class=" flex items-center justify-center"> Register </div>
   
              </div>
