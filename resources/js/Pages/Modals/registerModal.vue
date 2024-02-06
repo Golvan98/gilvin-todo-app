9c262e377b0ff9ff98a1b42ffad6434f19b37972
Golvan98 <gilvinsz@gmail.com>
2024-02-06 07:10:04 -0500

    added composer settings

--- resources/js/Pages/Modals/registerModal.vue
+++ resources/js/Pages/Modals/registerModal.vue
@@ -1,7 +1,7 @@
 <template>
     <div  class="fixed inset-0 z-50 flex items-center justify-center bg-gray-950 bg-opacity-95 w-full ">
           <div class="w-1/4 flex flex-col items-center justify-center text-gray-600 border border-gray-900 bg-white h-2/5">
-              <div id="closeSection" class="w-full h-10-percent flex items-start justify-end"> 
+              <div id="closeSection" class="w-full h-20-percent flex items-start justify-end"> 
                   <button @click="closeRegModal" class=" text-black hover:text-red-400 focus:outline-none">
                       <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path> 
