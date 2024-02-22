b14654a0c6ec5c3372145c5b5b6d0c544a4433ec
Golvan98 <gilvinsz@gmail.com>
2024-02-22 07:57:39 -0500

    adjusted form field sizes, found a password overlapping layout bug

--- resources/js/Pages/Modals/registerModal.vue
+++ resources/js/Pages/Modals/registerModal.vue
@@ -1,7 +1,8 @@
 <template>
     <div  class="fixed inset-0 z-50 flex items-center justify-center bg-gray-950 bg-opacity-95 w-full ">
-          <form @submit.prevent="register" class="w-1/4 flex flex-col items-center justify-center text-gray-600 border border-gray-900 bg-white h-2/5">
-              <div id="closeSection" class="w-full h-20-percent flex items-start justify-end"> 
+          <form @submit.prevent="register" class="w-1/4 flex flex-col items-center justify-center text-gray-600 border border-gray-900 bg-white h-3/5">
+
+              <div id="closeSection" class="w-full h-10-percent flex items-start justify-end"> 
                   <button @click="closeRegModal" class=" text-black hover:text-red-400 focus:outline-none">
                       <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path> 
@@ -9,26 +10,35 @@
                   </button>
               </div>
   
-              <div id="logo" class="w-full h-10-percent flex items-center justify-center sm:text-xs md:text-md lg:text-2xl mb-1"> 𝔾ℙ𝕄</div>
-             <div id="2ndBlock" class="w-4/5 flex flex-col items-center justify-center h-full">
-              
-              <input type="hidden" name="_token" :value="csrfToken"> <!-- CSRF token field -->
-              
-              <label for="name"></label><input id="name" v-model="form.name" type="text" placeholder="name" class="bg-gray-300 w-4/5 mb-2 h-10-percent mx-auto text-xs lg:py-4 md:py-0.5 sm:py-0.5"> 
-              <div v-if="form.errors.name" class="text-xs text-red-500 flex justify-start w-4/5"> {{form.errors.name}}</div>
+              <div id="logo" class="w-full h-20-percent flex items-center justify-center sm:text-xs md:text-2xl lg:text-4xl mb-1"> 𝔾ℙ𝕄</div>
 
-              <label for="email"></label><input id="email" v-model="form.email" type="text" placeholder="email" class="bg-gray-300 w-4/5 mb-2 h-10-percent mx-auto text-xs lg:py-4 md:py-0.5 sm:py-0.5"> 
-              <div v-if="form.errors.email" class="text-xs text-red-500 flex justify-start w-4/5"> {{form.errors.email}}</div>
+              <div id="2ndBlock" class="w-4/5 flex flex-col items-center justify-center h-70-percent">
+                      <input type="hidden" name="_token" :value="csrfToken"> <!-- CSRF token field -->
 
-              <label for="password"></label><input id="password" v-model="form.password" type="text" placeholder="password" class="bg-gray-300 w-4/5 h-10-percent mx-auto mb-2 text-xs lg:py-4 md:py-0.5 sm:py-0.5"> 
-              <div v-if="form.errors.password" class="text-xs text-red-500 flex justify-start w-4/5"> {{form.errors.password}}</div>
+                      <div id="nameSection" class="h-20-percent my-4">
+                          <label for="name"></label>
+                          <input id="name" v-model="form.name" type="text" placeholder="name" class="lg:p-4 md:p-0.5">
+                          <span v-if="form.errors.name" class="text-xs text-red-500 flex justify-start w-full"> {{form.errors.name}}</span>
+                      </div>
 
-              <div class="flex items-center justify-center mb-8"> <button type="submit"> Register </button> </div>
+                      <div id="emailSection" class="h-20-percent my-4">
+                          <label for="email"></label>
+                          <input id="email" v-model="form.email" type="text" placeholder="email" class="lg:p-4 md:p-0.5">
+                          <span v-if="form.errors.email" class="text-xs text-red-500 flex justify-start w-full"> {{form.errors.email}}</span>
+                      </div>
+
+                      <div id="passwordSection" class="h-20-percent my-4">
+                          <label for="password"></label>
+                          <input id="password" v-model="form.password" type="text" placeholder="password" class="lg:p-4 md:p-0.5">
+                          <span v-if="form.errors.password || form.errors.email" class="text-xs text-red-500 flex "> {{form.errors.password}}</span>
+                      </div>
+
+                      <div class="flex h-20-percent items-center justify-center mb-8">
+                          <button type="submit">Register</button>
+                      </div>
+                </div>
   
-             
-             </div>
-  
-            </form>
+          </form>
       </div>
 </template>
 
@@ -87,6 +97,14 @@ const closeRegModal = () => {
 
   .h-20-percent{
       height: 20%;
+  }
+
+  .h-25-percent{
+      height: 20%;
+  }
+
+  .h-60-percent{
+      height: 60%;
   }
 
   @media screen and (min-width: 768px)
