28a90141ff386998cdd73fecaa40bf4487861f00
Golvan98 <gilvinsz@gmail.com>
2024-03-05 06:42:39 -0500

    updated projects page UI

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Project;
 use App\Models\Task;
+use App\Models\User;
+use App\Models\ProjectUser;
 
 class ProjectController extends Controller
 {
@@ -14,11 +16,16 @@ class ProjectController extends Controller
 
         $projects = Project::all(); //havent tested this query yet
         $tasks = Task::all();
+        $users = User::all();
+        $projectUsers = ProjectUser::all();
         
+    
         return inertia('projects' ,
     [
         'projects' => $projects,
-        'tasks' => $tasks
+        'tasks' => $tasks,
+        'users' => $users,
+        'projectUsers' => $projectUsers
     ]);
 
    
--- resources/js/Pages/projects.vue
+++ resources/js/Pages/projects.vue
@@ -6,11 +6,10 @@
 
 <div id="firstBlock" class="bg-white flex items-center justify-center mx-auto mr-5-pct ml-5-pct w-full h-full">
 
- <div id="secondBlock" class="bg-inherit h-4/5 w-full grid grid-cols-5 grid-rows-6 text-white">
+ <div id="secondBlock" class="bg-inherit h-full w-full grid grid-cols-5 grid-rows-6 text-white">
 
 
-
-    <div id="projectNavBarContainer" class="col-start-2 col-end-5 bg-inherit row-start-1 grid grid-cols-6 mt-2">
+    <div id="projectNavBarContainer" class="col-start-2 col-end-5 bg-inherit row-start-1 grid grid-cols-6 mt-2 row-span-1">
 
         <div id="projectNavBar" class="bg-indigo-300 col-start-2 col-end-6 rounded-lg mb-2">
             
@@ -19,18 +18,19 @@
     </div>
 
     <div id="toDoRow" class="row-start-2 row-span-5 col-start-2 bg-indigo-300 rounded-sm overflow-y-auto">
-     <div v-for="project in projects" :key="projects.id" class="h-1/6">
-      {{ project.project_name }}  
-      
-      <span v-if="task.project_id==project.id"> I am  {{ task.name }} for {{ project.project_name }}</span>
-      
+
+     <div v-for="project in projects" :key="projects.id" class="h-1/5 w-3/4 bg-white text-black mx-auto mt-2 rounded-lg">
+        <div class="mx-auto w-3/4 h-1/4 font-bold">  {{ project.project_name }}  </div>   
+        
+        <div id="projectDescription" class="mx-auto w-4/5 h-3/4 text-xs"> {{ project.project_description }}</div>
      </div>
-      
-      
+    
     </div>
 
-    <div id="inProgressRow" class="row-start-2 row-span-5 col-start-3 bg-indigo-300 rounded-sm overflow-y-auto">
-      
+    <div id="inProgressRow" class="row-start-2 row-span-5 col-start-3 bg-indigo-300 rounded-sm overflow-y-auto ">
+      <div v-for="user in users" :key="users.id">
+          {{ user.name }}
+      </div>      
     </div>
 
     <div id="completeRow" class="row-start-2 row-span-5 col-start-4 bg-indigo-300 rounded-sm overflow-y-auto">
@@ -68,12 +68,17 @@
 
 <script setup>
 
+import { ref, watch , computed  } from 'vue'
 import MainLayout from '@/Pages/Layouts/MainLayout.vue'
 
 const props = defineProps ({
   projects: Object,
-  tasks: Object
+  tasks: Object,
+  users: Object,
+  projectUsers: Object
 })
+
+const selectedProject = ref(null);
 </script>
 
 <style>
