c886b85e3580fb4e49b5ee35638704cc90a3ef01
Golvan98 <gilvinsz@gmail.com>
2024-05-18 12:04:41 -0400

    made UI changes to add task modal

--- resources/js/Pages/Modals/addTaskModal.vue
+++ resources/js/Pages/Modals/addTaskModal.vue
@@ -3,37 +3,38 @@
 
           <form @submit.prevent="createTask" class="bg-white w-1/4 h-2/5 flex-nowrap text-gray-600 border border-gray-900 ">
   
-           <div class="w-full h-3/5 bg-green-500">
-            <div id="closeSection" class="w-full flex h-10-percent items-start justify-end"> 
-                  <button @click="closeAddTaskModal" class=" text-black hover:text-red-400 focus:outline-none">
-                      <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path> 
-                      </svg>
-                  </button>
-              </div>
-            <div id="formLabel" class="whitespace-nowrap h-45-percent flex items-center  justify-center"> Task Name for project {{ selectedProjectId }} </div>
-            <div id="taskForm" class="h-45-percent flex items-center justify-center">
-                <input v-model="form.name" id="name" type="text" class="w-3/4 h-1/2" placeholder="Task name here">     
-            </div>
+           <div class="w-full h-1/2">
+
+                <div id="closeSection" class="w-full flex h-10-percent items-start justify-end"> 
+                    <button @click="closeAddTaskModal" class=" text-black hover:text-red-400 focus:outline-none">
+                        <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path> 
+                        </svg>
+                    </button>
+                </div>
+                <div id="formLabel" class="whitespace-nowrap h-45-percent flex items-center  justify-center"> Task Name for project {{ selectedProjectId }} </div>
+                <div id="taskForm" class="h-45-percent flex items-start justify-center">
+                    <input v-model="form.name" id="name" type="text" class="w-3/4 h-1/2" placeholder="Task name here">     
+                </div>
         
            </div>
     
 
-           <div class="w-full h-2/5 bg-inherit">
+           <div class="w-full h-1/2 bg-inherit flex items-center justify-center flex-col">
 
-            <div class="h-1/4 flex items-start justify-center"> Task Status</div>
-            <div class="h-1/4 flex items-start justify-center">
-                <select v-model="form.status" id="status">
-                <option value="Pending">Pending</option>
-                <option value="In Progress">In Progress</option>
-                <option value="Complete">Complete</option>   
+                <div id="selectStatusLabel" class="h-1/3 w-full flex items-center justify-center"> Task Status</div>
+              
+                <select v-model="form.status" type="text"  placeholder="Project Name" id="status" class="h-1/3 w-3/4 flex items-start justify-center">
+                    <option id="option1" value="Pending">Pending</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Complete">Complete</option>   
                 </select>
-            </div>
-            
+               
+                
 
-            <div class="h-1/2 flex items-center justify-center"> 
-                <button class="p-1 bg-gray-300"> Create Task </button>
-            </div>  
+                <div class="h-1/2 w-full flex items-center justify-center"> 
+                    <button class="bg-indigo-300 w-1/4 h-1/2 text-white whitespace-nowrap"> Create Task </button>
+                </div>  
 
            </div>
           
@@ -81,36 +82,63 @@ const closeAddTaskModal = () => {
     height:45%;
 }
 
-@media screen and (min-width:901px) {
-    /* 200% and below if maxwidth:1199px*/
-    #formLabel{
-    font-size: 18px;
+@media screen and (min-width:900px) {
+    /* 150% to 0% */
+    #formLabel,#taskForm,#selectStatusLabel,#selectStatus{
+    font-size: 20px;
    
     }   
+    #status{
+        font-size:8px;
+    }
 }
 
-@media screen and (min-width: 1px) and (max-width:899px){
-   /* 250%-300% */
+@media screen and (min-width:600px) and (max-width:899px){
+    /* 175 - 250% */
+    #formLabel,#taskForm,#selectStatusLabel,#selectStatus{
+    font-size: 10px;
+        
+    }   
+    #formLabel{
+        margin-top:4px;
+    }
 
-   #formLabel{
-    font-size:11px;
-    margin-top:4px;
-   }
-   #taskForm{
-    font-size:4px;
-   }
-
+    #status {
+    font-size: 8px; /* Adjust the font size to your preference */
 }
 
-@media screen and (min-width:400px) and (max-width:480px){
-    /* 400 px */
-
+/* Style the options within the select element */
 }
 
-@media screen and (min-width:0px) and (max-width:400px){
-    /* 500 px */
-   
+@media screen and (min-width:481px) and (max-width:599px){
+    #formLabel,#taskForm,#selectStatusLabel,#selectStatus{
+    font-size: 7px;
+    }   
 
+    #status{
+        font-size:8px;
+    }
+    #name{
+        font-size:8px;
+    }
+}
+
+@media screen and (min-width:1px) and (max-width:480px){
+    
+    #formLabel,#taskForm,#selectStatusLabel,#selectStatus{
+    font-size: 7px;
+    }   
+
+    #selectStatusLabel{
+        display:none;
+    }
+
+    #status{
+        font-size:4px;
+    }
+    #name{
+        font-size:4px;
+    }
 }
 
 
